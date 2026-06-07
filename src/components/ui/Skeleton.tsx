21f74fd22112819b5186e33ec7f55099b8c5ca99
Omar Basheer <omasheer@gmail.com>
2026-06-07 12:52:37 +0000

    add edit sessions feature, remove upcoming tag on cousrse card, add cohort code to course card, change next upcoming date to show today if next session falls on currrent date

--- src/components/ui/Skeleton.tsx
+++ src/components/ui/Skeleton.tsx
@@ -18,7 +18,10 @@ export function CourseCardSkeleton() {
       <div className="flex-1 min-w-0 flex flex-col gap-2">
         <Skeleton className="h-3 w-16" />
         <Skeleton className="h-4 w-48" />
-        <Skeleton className="h-3 w-32" />
+        <div className="flex items-center gap-3">
+          <Skeleton className="h-3 w-20" />
+          <Skeleton className="h-3 w-24" />
+        </div>
       </div>
       <Skeleton className="h-4 w-8 flex-shrink-0" />
     </div>
